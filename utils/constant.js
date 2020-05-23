4ca429fe5c7003c50db39d280bbc2c9983c90929
Hamza Ghufran <hamzazeb95@gmail.com>
2020-05-23 20:34:44 +0530

    Update

--- utils/constant.js
+++ utils/constant.js
@@ -1,6 +1,6 @@
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const DOB_REGEX = /^((0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2})*$/;
-const PROJECT_ID = 'newagent-htycvu'; //https://dialogflow.com/docs/agents#settings
+const PROJECT_ID = 'newagent-htycvu'; 
 const SESSION_ID = '123456';
 const LANG_CODE = 'en-US';
 
